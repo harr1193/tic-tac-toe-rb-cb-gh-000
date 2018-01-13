4e14a865e4aef7bde6114fe239dd9568a3833ec8
Kyle Harris <k_harris95@live.com>
2018-01-13 06:56:40 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -37,9 +37,4 @@ def turn(board)
   puts "Please enter 1-9:"
   input = gets.strip
   index = input_to_index(input)
-  if valid_move(board, index)
 end
-
-board = ["X", " ", " ", " ", " ", " ", " ", " " ," "]
-
-display_board(board)
