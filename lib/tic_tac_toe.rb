c7d4ea0a53d8cc677545e0775737f4a006a69637
Kyle Harris <k_harris95@live.com>
2018-01-13 07:08:31 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -52,6 +52,40 @@ def current_player(board)
   count % 2 == 0 ? "X" : "O"
 end
 
-board = ["X", " ", " ", " ", " ", " ", " ", " " ," "]
+def won?(board)
+  WIN_COMBINATIONS.each do |win_combination|
+    win_index_1 = win_combination[0]
+    win_index_2 = win_combination[1]
+    win_index_3 = win_combination[2]
 
-display_board(board)
+    pos_1 = board[win_index_1]
+    pos_2 = board[win_index_2]
+    pos_3 = board[win_index_3]
+    all_pos = [win_index_1, win_index_2, win_index_3].all? do |index|
+      position_taken?(board, index)
+    end
+
+    ((return win_combination) if [pos_1, pos_2, pos_3].uniq.count == 1) if all_pos
+  end
+  return false
+end
+
+def full?(board)
+  num = [0, 1, 2, 3, 4, 5, 6, 7, 8]
+  all_pos = num.all? do |index|
+    position_taken?(board, index)
+  end
+end
+
+def draw?(board)
+  full?(board) && !won?(board)
+end
+
+def over?(board)
+  won?(board) || draw?(board) || full?(board)
+end
+
+def winner(board)
+  winner = won?(board)
+  won?(board) ? board[winner[0]] : nil
+end
